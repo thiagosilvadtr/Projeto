3a315fcf1844f0ab61a038d42582f687f5450e47
thiago dutra <thiago.silva2210@outlook.com>
2022-02-15 22:15:29 -0300

    alteration v1

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,32 @@
 //smooth scroll for internal links
 $(".custom").click(function (e) {
-    e.preventDefault();
-    let link = $(this).attr("href"),
-        targetOffset = $(link).offset().top,
-        menu = $("nav").innerHeight();
-    $('html, body').animate({
-        scrollTop: targetOffset - menu
-    }, 500);
+  e.preventDefault();
+  let link = $(this).attr("href"),
+    targetOffset = $(link).offset().top,
+    menu = $("nav").innerHeight();
+  $("html, body").animate(
+    {
+      scrollTop: targetOffset - menu,
+    },
+    500
+  );
 });
 
 //external links
-const linkExt = document.querySelectorAll(".card")[0];
-linkExt.addEventListener("click", openLink, false)
+let linkExt = document.querySelectorAll(".card")[0];
+linkExt.addEventListener("click", openLink, false);
 function openLink() {
-    window.open("https://monitordasdoacoes.netlify.app/pt", "_blank")
+  window.open("https://monitordasdoacoes.netlify.app/pt", "_blank");
 }
 
+linkExt = document.querySelectorAll(".card")[1];
+linkExt.addEventListener("click", openLink, false);
+function openLink() {
+  window.open("https://digitaldrumsplay.netlify.app/", "_blank");
+}
+
+linkExt = document.querySelectorAll(".card")[2];
+linkExt.addEventListener("click", openLink, false);
+function openLink() {
+  window.open("https://previsaodotempotd.netlify.app/", "_blank");
+}
